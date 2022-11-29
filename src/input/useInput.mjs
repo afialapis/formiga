11c962758b8f75e360e4b6eaaa5094154ec037c9
afialapis <devel@afialapis.com>
2022-11-29 11:43:15 +0100

    inputRef as useRef

--- src/input/useInput.mjs
+++ src/input/useInput.mjs
@@ -1,4 +1,4 @@
-import {useEffect, useState, useCallback} from 'react'
+import {useEffect, useState, useCallback, useRef} from 'react'
 import {log_input} from '../helpers/log.mjs'
 import useCheckProps from './checkers/useCheckProps.mjs'
 import useInputFilter from './inputFilter/useInputFilter.mjs'
@@ -16,76 +16,90 @@ const useInput = (props) => {
         doRepeat, doNotRepeat, decimals, 
         inputFilter, feedback}= props
   
+  const inputRef = useRef()
+  
   const [firstValidated, setFirstValidated]= useState(false)
-  const [inputNode, setInputNode]= useState(undefined)
+  //const [inputNode, setInputNode]= useState(undefined)
   const [validity, validateInput, setCustomValidity]  = useInputValidity(transformValue, checkValue, allowedValues, disallowedValues, doRepeat, doNotRepeat, decimals, feedback)
   
   //
   // Specific effect to check props consistency. Just DEV time
   //
-  useCheckProps(inputNode, doRepeat, doNotRepeat, inputFilter)
+  useCheckProps(/*inputNode*/ inputRef?.current, doRepeat, doNotRepeat, inputFilter)
 
   //
   // Attaches input filters when needed
   //
-  useInputFilter(inputNode, inputFilter)
+  useInputFilter(/*inputNode*/ inputRef?.current, inputFilter)
 
   //
   // Ensures checkboxes value
   //
-  useCheckboxEnsure(inputNode)
+  useCheckboxEnsure(/*inputNode*/ inputRef?.current)
 
-  const inputRef = useCallback(node => {
-    if (node!=null) {
-      log_input(node, 'inputRef callback')
-
-      setInputNode(node)
-    }
-  }, [])  
+  //  const inputRef = useCallback(node => {
+  //    if (node!=null) {
+  //      log_input(node, 'inputRef callback')
+  //
+  //      setInputNode(node)
+  //    }
+  //  }, [])  
 
   useEffect(() => {
-    if (inputNode!=undefined) {
+    if (/*inputNode*/ inputRef?.current!=undefined) {
       if (!firstValidated) {
         setFirstValidated(true)
-        validateInput(inputNode)
+        validateInput(/*inputNode*/ inputRef.current)
       }
 
-      const removeAllChangeListeners = attachInputValidationListener(inputNode, validateInput)
+      const removeAllChangeListeners = attachInputValidationListener(/*inputNode*/ inputRef.current, validateInput)
       return removeAllChangeListeners
 
     }
-  }, [inputNode, firstValidated, validateInput])
+  }, [/*inputNode*/ firstValidated, validateInput])
 
-  if (inputNode!=undefined) {
-    log_input(inputNode, 'render')
+  if (/*inputNode*/ inputRef?.current!=undefined) {
+    log_input(/*inputNode*/ inputRef.current, 'render')
   }
 
-  const forceValidation = useCallback(() => {
-    if (inputNode!=undefined) {
-      validateInput(inputNode)
+  const validate = useCallback(() => {
+    if (/*inputNode*/ inputRef?.current!=undefined) {
+      validateInput(/*inputNode*/ inputRef?.current)
     }
-  }, [inputNode, validateInput])
+  }, [/*inputNode*/ validateInput])
 
-  const forceValue = useCallback((v) => {
-    if (inputNode!=undefined) {
-      inputNode.value = v
+  const setValue = useCallback((v) => {
+    if (/*inputNode*/ inputRef?.current!=undefined) {
+      /*inputNode*/ inputRef.current.value = v
     }
-  }, [inputNode])
+  }, [/*inputNode*/])
 
-  const forceValidity = useCallback((msg) => {
-    if (inputNode!=undefined) {
-      setCustomValidity(inputNode, msg)
+  const setValidity = useCallback((msg) => {
+    if (/*inputNode*/ inputRef?.current!=undefined) {
+      setCustomValidity(/*inputNode*/ inputRef.current, msg)
     }
-  }, [inputNode, setCustomValidity])
+  }, [/*inputNode*/ setCustomValidity])
+  
+  const dipatchEvent = useCallback((type, data, bubbles= false) => {
+    if (inputRef?.current==undefined) {
+      return
+    }
+    const inputEvent = new CustomEvent(type, {
+      bubbles,
+      detail: data || {}
+    })
+    inputRef.current.dispatchEvent(inputEvent)
 
+  }, [])
   
   return [inputRef, {
     valid: validity==='', 
     message: validity, 
-    inputNode,
-    forceValidation,
-    forceValue,
-    forceValidity
+    //inputNode,
+    validate,
+    setValue,
+    setValidity,
+    dipatchEvent
   }]
 }
 
