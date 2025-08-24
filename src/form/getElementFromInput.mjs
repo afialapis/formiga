c43a8910ff7d5e9eb13405010e83e207e2644e38
afialapis <devel@afialapis.com>
2025-08-24 09:46:45 +0200

    probably fixing

--- src/form/getElementFromInput.mjs
+++ src/form/getElementFromInput.mjs
@@ -6,7 +6,12 @@ import {
 export default (node) => {
   const name = node.name
   const type = node.type || node.getAttribute('type')
-  const validationMessage= node.validationMessage
+  
+  // For some reason, node.validationMessage is not reliable when 
+  //    accessing it from here.
+  // const validationMessage= node.validationMessage
+  const validationMessage= node.getAttribute('data-formiga-validity')
+
   const value = getInputValue(node)
   const valid = validationMessage==''
   const originalValue = getOriginalValueFromNode(node)
